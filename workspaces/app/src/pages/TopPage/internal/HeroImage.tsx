a47b119d9971efada242b53e7404014f20f3432f
mazrean <shunsuke.w04@gmail.com>
2024-03-23 19:55:05 +0900

    cache

--- workspaces/app/src/pages/TopPage/internal/HeroImage.tsx
+++ workspaces/app/src/pages/TopPage/internal/HeroImage.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-import { IMAGE_SRC } from './ImageSrc';
+import { getImageUrl } from '../../../lib/image/getImageUrl';
 
 const _Wrapper = styled.div`
   aspect-ratio: 16 / 9;
@@ -15,9 +15,13 @@ const _Image = styled.img`
 `;
 
 export const HeroImage: React.FC = () => {
+  const dpr = window.devicePixelRatio;
+  const width = window.innerWidth > 1024 ? 1024 : window.innerWidth;
+  const height = (width / 16) * 9;
+  const src = getImageUrl({ format: 'webp', height: height*dpr, imageId: '4a2c68cf-53eb-4b66-a4de-e1c2bf1c13c8', width: width*dpr});
   return (
     <_Wrapper>
-      <_Image alt="Cyber TOON" src={IMAGE_SRC} />
+      <_Image alt="Cyber TOON" src={src} />
     </_Wrapper>
   );
 };
