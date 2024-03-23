c1376805e78731af2dff3254fc04ad2aa8e4f171
mazrean <shunsuke.w04@gmail.com>
2024-03-23 16:22:19 +0900

    optimize inject data

--- workspaces/server/src/routes/ssr/index.tsx
+++ workspaces/server/src/routes/ssr/index.tsx
@@ -3,17 +3,17 @@ import fs from 'node:fs/promises';
 import { Hono } from 'hono';
 import { HTTPException } from 'hono/http-exception';
 import jsesc from 'jsesc';
-import moment from 'moment-timezone';
+//import moment from 'moment-timezone';
 import ReactDOMServer from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom/server';
 import { ServerStyleSheet } from 'styled-components';
 import { unstable_serialize } from 'swr';
 
 import { featureApiClient } from '@wsh-2024/app/src/features/feature/apiClient/featureApiClient';
-import { rankingApiClient } from '@wsh-2024/app/src/features/ranking/apiClient/rankingApiClient';
-import { releaseApiClient } from '@wsh-2024/app/src/features/release/apiClient/releaseApiClient';
+//import { rankingApiClient } from '@wsh-2024/app/src/features/ranking/apiClient/rankingApiClient';
+//import { releaseApiClient } from '@wsh-2024/app/src/features/release/apiClient/releaseApiClient';
 import { ClientApp } from '@wsh-2024/app/src/index';
-import { getDayOfWeekStr } from '@wsh-2024/app/src/lib/date/getDayOfWeekStr';
+//import { getDayOfWeekStr } from '@wsh-2024/app/src/lib/date/getDayOfWeekStr';
 
 import { INDEX_HTML_PATH } from '../../constants/paths';
 
@@ -23,19 +23,8 @@ async function createInjectDataStr(): Promise<Record<string, unknown>> {
   const json: Record<string, unknown> = {};
 
   {
-    const dayOfWeek = getDayOfWeekStr(moment());
-    const releases = await releaseApiClient.fetch({ params: { dayOfWeek } });
-    json[unstable_serialize(releaseApiClient.fetch$$key({ params: { dayOfWeek } }))] = releases;
-  }
-
-  {
     const features = await featureApiClient.fetchList({ query: {} });
     json[unstable_serialize(featureApiClient.fetchList$$key({ query: {} }))] = features;
-  }
-
-  {
-    const ranking = await rankingApiClient.fetchList({ query: {} });
-    json[unstable_serialize(rankingApiClient.fetchList$$key({ query: {} }))] = ranking;
   }
 
   return json;
@@ -55,12 +44,25 @@ async function createHTML({
   const content = htmlContent
     .replaceAll('<div id="root"></div>', `<div id="root">${body}</div>`)
     .replaceAll('<style id="tag"></style>', styleTags)
+    .replaceAll(
+      '<script id="inject-data" type="application/json"></script>',
+      `<script id="inject-data" type="application/json">
+        ${jsesc(injectData, {
+          isScriptContext: true,
+          json: true,
+          minimal: true,
+        })}
+      </script>`,
+    );
 
   return content;
 }
 
 app.get('*', async (c) => {
-  const injectData = await createInjectDataStr();
+  let injectData: Record<string, unknown> = {};
+  if (c.req.path === '/') {
+    injectData = await createInjectDataStr();
+  }
   const sheet = new ServerStyleSheet();
 
   try {
