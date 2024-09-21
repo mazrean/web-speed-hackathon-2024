ed454c7e63e8c299177c4acf81c197aaa3894d98
mazrean <shunsuke.w04@gmail.com>
2024-09-22 01:40:54 +0900

    momentjs削除

--- workspaces/app/src/lib/date/getDayOfWeekStr.ts
+++ workspaces/app/src/lib/date/getDayOfWeekStr.ts
@@ -1,12 +1,5 @@
-import type moment from 'moment-timezone';
-
 const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'] as const;
 
-export const getDayOfWeekStr = (date: moment.Moment) => {
-  const dayOfWeek = date.day();
-  const dayStr = days.at(dayOfWeek);
-  if (dayStr == null) {
-    throw new Error('dayOfWeek is invalid');
-  }
-  return dayStr;
+export const getDayOfWeekStr = (date: Date) => {
+  return days[date.getDay()] as (typeof days)[number];
 };
